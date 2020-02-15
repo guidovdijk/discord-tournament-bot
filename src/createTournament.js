b1de88b251ad1001a746cc5a70501d4fb9cbc93c
Guido van Dijk <GuidoD@Guido-van-Dijk.local>
2020-02-15 16:38:00 +0100

    Refactor createTournament.js

--- src/createTournament.js
+++ src/createTournament.js
@@ -1,70 +1,64 @@
-import createCollection from './firebase/createCollection';
-
 class createTournament {
-    constructor(db, path, msg, args){
+    constructor(db, collection, msg, args){
         this.db = db;
-        this.path = path,
+        this.collection = collection,
         this.msg = msg,
         this.args = args,
         this.replies = {
-            formatError: "Please provide the following details: to the command: <team size>  <participation limit>",
+            formatError: "Please provide the following details: to the command: <team size>  <minimum required participators>",
             argsError: {
                 limit: "Error: Team size exceeded the limit of 5",
                 uneven: `Tournament will be created, but there is an uneven amount of teams.\nPlease make sure it is even so everyone can play.`,
             },
-            created: {
-                color: 0x0da258,
-                title: "**Tournament is created, please pick your teammate(s)!**",
-                fields: [
-                    {
-                        name: "\n**To add a team:**",
-                        value: "```\n!tournament-team <your league name>, <your discord name>, <partner's league name>, <partner's discord name>```\n"
-                    },
-                    {
-                        name: "\n**Want to change partners? Run the command below overwrite**",
-                        value: "```\n!tournament-team```"
-                    },
-                    {
-                        name: "**To see all teams**",
-                        value: "```\n!tournament-show```"
-                    }
-                ]
-            },
         },
-        this.init();
+        this.checkArgs();
     }
 
-    init(){
-        this.checkFormat();
-    }
-
-    checkFormat(){
+    checkArgs(){
         if(this.args.length !== 2){
             this.msg.reply(this.replies.formatError);
             return;
         } else {
-            this.checkArgs();
+            this.checkFormat();
         }
     }
 
-    checkArgs(){
-        const [teamSize, participationLimit] = [this.args[0], this.args[1]];
+    checkFormat(){
+        const [teamSize, participators] = [this.args[0], this.args[1]];
 
         if(teamSize > 5){
             this.msg.reply(this.replies.argsError.limit);
             return;
         }
 
-        if(participationLimit % 2 == 1){
+        if(participators % 2 == 1){
             this.msg.reply(this.replies.argsError.uneven);
         }
 
-        this.createTournament();
+        this.createTournament(teamSize, participators);
     }
 
-    createTournament(){
-        createCollection(this.db, this.msg);
-        this.msg.channel.send({embed: this.replies.created});
+    createTournament(teamSize, participators){
+        const createdRepley = {
+            color: 0x0da258,
+            title: `**${teamSize}VS${teamSize} Tournament is created, please pick your teammate(s)!**`,
+            fields: [
+                {
+                    name: "\n**To add a team:** (Want to change partners? Run the command again to overwrite)",
+                    value: "```\n!tournament-team <your league name> <your discord name> <partner's league name> <partner's discord name>```\n"
+                },
+                {
+                    name: "**To see all teams**",
+                    value: "```\n!tournament-show```"
+                }
+            ]
+        };
+        this.db.collection(this.collection).doc("data").set({
+            teamSize: parseInt(teamSize),
+            minimumRequired: parseInt(participators),
+            isOpen: true,
+        });
+        this.msg.channel.send({embed: createdRepley});
     }
 }
 
